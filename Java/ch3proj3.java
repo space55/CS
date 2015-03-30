b7b8d45e3d692f1a6ff79dc06e4fdbd20088409e
space55 <msvetty@new-host.home>
2015-03-30 19:24:01 -0400

    Ch3Proj3 Update

--- Java/ch3proj3.java
+++ Java/ch3proj3.java
@@ -1,9 +1,41 @@
+//Chapter 3 Project #3
+//Name: Eamonn Nugent
 import java.util.Scanner;
 
 public class ch3proj3
 {
 	public static void main(String[] args)
 	{
-		
+		Scanner in = new Scanner(System.in);
+		boolean addPassenger = true;
+		while (addPassenger)
+		{
+			System.out.println("Would you like to check a fare? Please answer \"Y\" or \"N\"");
+			String addPass = in.nextLine();
+			if (addPass.equalsIgnoreCase("N"))
+			{
+				addPassenger = false;
+			}
+			double age = 18;
+			boolean isValid = false;
+			int busFare = 80;
+			while (!isValid)
+			{
+				System.out.println("What is your age?");
+				try
+				{
+					age = in.nextDouble();
+					isValid = true;
+				}
+				catch (Exception e)
+				{
+				}
+			}
+			if (age < 18)
+			{
+				busFare = 40;
+			}
+			System.out.println("Your bus fare is $0." + busFare);
+		}
 	}
 }
