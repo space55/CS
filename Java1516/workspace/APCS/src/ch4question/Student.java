bb8c40eeb993f81c1b0955e3feca46fbee620f7c
space55 <elg.nugent@gmail.com>
2015-12-15 21:43:46 -0500

    12/15/15 Commit
    Before I wipe my laptop!

---
+++ Java1516/workspace/APCS/src/ch4question/Student.java
@@ -0,0 +1,11 @@
+package ch4question;
+
+public class Student
+{
+	private int age;
+
+	public int getAge()
+	{
+		return age;
+	}
+}
---
+++ Java1516/workspace/APCS/src/ch4question/Test.java
@@ -0,0 +1,10 @@
+package ch4question;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		Student kathy = new Student();
+		System.out.println(kathy.getAge());
+	}
+}
